5a31ece5ca120b3c0f63eb516404ce8223225929
Viny Selopal <viny0698@gmail.com>
2024-12-11 09:51:28 +0530

    debug client requests and form

--- script.js
+++ script.js
@@ -1,28 +1,54 @@
-const questionsList = document.getElementById("questions_list")
-document.getElementById("submit_button").addEventListener('click', (event) => {
-    const response = []
-    questionsList.children.forEach(q => {
-        response.push({
-            question: q.querySelector("p"),
-            answer: q.querySelector("select").value
-        })
-    })
-})
+(async () => {
+  const questionsList = document.getElementById("questions_list");
+  document
+    .getElementById("submit_button")
+    .addEventListener("click", async (event) => {
+      const body = {
+        mentor_id: 1,
+      };
+      const children = questionsList.children;
+      [...children].forEach((q) => {
+        console.log("id", q.id);
+        body.response = {
+          question_id: q.id,
+          answer: q.querySelector("select")[0].value,
+          score: q.querySelector("select")[1].value,
+        };
+      });
+      const res = await fetch("http://localhost:3000/mentor_answers", {
+        method: "POST",
+        body: JSON.stringify(body),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+    });
 
-const questions = fetch("localhost:3000", {
-    method: "GET"
-}).then(res => {
-    console.log("res", res)
-    Object.keys(res).forEach(q => {
-        const innerHTML = `
-            <li id=${res[q].id}>
-                <p>${res[q].question}</p>
-                <select>
-                    ${
-                        res[q].answers.map(a => `<option>${a}</option>`).join('')
-                    }
-                <select>
-            <li>
-        `
-    })
-})
+  const res = await fetch("http://localhost:3000/questions", {
+    method: "GET",
+  });
+  console.log("res", res);
+  const body = await res.json();
+
+  console.log("body", body);
+  let innerHTML = ``;
+  Object.keys(body).forEach((q) => {
+    innerHTML += `
+                <li id=${body[q].question_id}>
+                    <p>${q}</p>
+                    <select>
+                        ${body[q].answers
+                          .map((a) => `<option>${a}</option>`)
+                          .join("")}
+                    </select>
+                    <select>
+                        ${[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+                          .map((n) => `<option>${n}</option>`)
+                          .join("")}
+                    </select>
+                </li>
+            `;
+  });
+  console.log("inner html", innerHTML);
+  questionsList.innerHTML = innerHTML;
+})();
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const { Client } = require("pg");
 
@@ -14,36 +15,41 @@ const client = new Client({
   await client.connect();
 })();
 
+app.use(cors({ origin: true }));
 app.use(express.json());
 
 app.get("/questions", async (req, res, next) => {
-  const raw_questions =
-    await (await client.query(`SELECT questions.name as question, questions.id as question_id, predefined_answers.answer
+  const raw_questions = await (
+    await client.query(`SELECT questions.name as question, questions.id as question_id, predefined_answers.answer
         FROM questions JOIN predefined_answers
-        ON questions.id = predefined_answers.question_id`)).rows;
+        ON questions.id = predefined_answers.question_id`)
+  ).rows;
   const questions = raw_questions.reduce((prev, curr) => {
     if (prev[curr.question]) prev[curr.question].answers.push(curr.answer);
-    else prev[curr.question] = {
+    else
+      prev[curr.question] = {
         question_id: curr.question_id,
-        answers: [curr.answer]
-    };
+        answers: [curr.answer],
+      };
+    return prev;
   }, {});
   console.log("questions", questions);
   res.json(questions);
 });
 
 app.post("/mentor_answers", async (req, res, next) => {
-  const mentor_response = req.body;
-
-  const response = await client.query(
+  const { mentor_id, response } = req.body;
+  console.log("body", req.body);
+  const db_response = await client.query(
     `INSERT INTO mentor_answers
     (answer, question_id, mentor_id, score)
     VALUES
-    (${mentor_response.answer},
-    ${mentor_response.question_id},
-    ${mentor_response.mentor_id},
-    ${mentor_response.score});`
+    (${response.answer},
+    ${response.question_id},
+    ${mentor_id},
+    ${response.score});`
   );
+  res.json(1);
 });
 
 app.post("/mentee_answers", async (req, res, next) => {
